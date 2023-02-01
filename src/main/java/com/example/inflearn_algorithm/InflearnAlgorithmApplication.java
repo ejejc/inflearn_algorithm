36a9bbcf20db9d04168f746ba41cece107a076de
eunjis <ejejc@naver.com>
2023-02-01 16:29:34 +0900

    Initial commit

---
+++ src/main/java/com/example/inflearn_algorithm/InflearnAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnAlgorithmApplication.class, args);
+    }
+
+}
