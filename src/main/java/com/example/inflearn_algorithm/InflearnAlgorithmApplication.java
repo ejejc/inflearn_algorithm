6d12b021eca53c61e29462db8b414c8c7a4bb12c
eunjis <ejejc@naver.com>
2023-02-01 19:02:16 +0900

    문자열 찾기 알고리즘.

--- src/main/java/com/example/inflearn_algorithm/InflearnAlgorithmApplication.java
+++ src/main/java/com/example/inflearn_algorithm/InflearnAlgorithmApplication.java
@@ -1,13 +1,20 @@
 package com.example.inflearn_algorithm;
 
+import com.example.inflearn_algorithm.chapter1.Ch01_01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class InflearnAlgorithmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InflearnAlgorithmApplication.class, args);
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char s = sc.next().charAt(0);
+        System.out.println(Ch01_01.FindCharacter(str, s));
     }
 
 }
---
+++ src/main/java/com/example/inflearn_algorithm/chapter1/Ch01_01.java
@@ -0,0 +1,38 @@
+package com.example.inflearn_algorithm.chapter1;
+
+import org.springframework.util.StringUtils;
+
+import java.util.Scanner;
+
+public class Ch01_01 {
+    /**
+     * 한개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램
+     *
+     * 대소문자를 구분하지 않으며 문자열의 길이는 100을 넘지 않는다.
+     *
+     * 입력
+     * - 첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다. (ex, Computercooler)
+     * - 두 번째 줄에 문자가 주어진다. (ex. c)
+     *
+     * 출력
+     * 2
+     */
+    public static int FindCharacter(String str, char c) {
+        int answer = 0;
+        // 대소문자 구별 안함
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
+
+        for (char x : str.toCharArray()) {
+            if (x == c) answer++;
+        }
+        return answer;
+    }
+
+    public static void prints() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char s = sc.next().charAt(0);
+        System.out.println(FindCharacter(str, s));
+    }
+}
