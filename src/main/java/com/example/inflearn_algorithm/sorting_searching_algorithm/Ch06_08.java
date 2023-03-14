58c6e45de5003b9a0079aed32def57f051ac8758
eunjis <ejejc@naver.com>
2023-03-14 21:22:13 +0900

    이분검색 알고리즘 풀이.

--- src/main/java/com/example/inflearn_algorithm/sorting_searching_algorithm/Ch06_08.java
+++ src/main/java/com/example/inflearn_algorithm/sorting_searching_algorithm/Ch06_08.java
@@ -31,4 +31,21 @@ public class Ch06_08 {
         }
         return result;
     }
+
+    // 이분검색 알고리즘
+    private static int solution2(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0, rt = n-1;
+        while (lt <= rt) {
+            int mid = (lt+rt)/2;
+            if (arr[mid]==m) {
+                answer = mid +1;
+                break;
+            }
+            if (arr[mid] > m) rt  = mid -1;
+            else lt = mid+1;
+        }
+        return answer;
+    }
 }
