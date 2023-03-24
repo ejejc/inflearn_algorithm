2d7209df40170dfd5eb5553af1016ba44144ab79
eunjis <ejejc@naver.com>
2023-03-24 20:54:49 +0900

    결정 알고리즘 뮤직비디오 용량 구하기.

--- src/main/java/com/example/inflearn_algorithm/InflearnAlgorithmApplication.java
+++ src/main/java/com/example/inflearn_algorithm/InflearnAlgorithmApplication.java
@@ -10,7 +10,7 @@ public class InflearnAlgorithmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InflearnAlgorithmApplication.class, args);
-        Ch01.prints();
+        Ch06_09.prints();
     }
 
 }
--- src/main/java/com/example/inflearn_algorithm/sorting_searching_algorithm/Ch06_09.java
+++ src/main/java/com/example/inflearn_algorithm/sorting_searching_algorithm/Ch06_09.java
@@ -5,6 +5,10 @@ import java.util.*;
 
 public class Ch06_09 {
 
+    /**
+     * 결정 알고리즘 - 이분 탐색 이용
+     *
+     */
     public static void prints() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -19,39 +23,32 @@ public class Ch06_09 {
     private static int solution(int n, int m, int[] arr) {
         int lt = Arrays.stream(arr).max().getAsInt();
         int rt = Arrays.stream(arr).sum();
-        int sum = 0;
-        int result = Integer.MAX_VALUE;
-        List<Integer> list = new ArrayList<>();
+        int mid = 0;
+        int result = 0;
         while (lt <= rt) {
-            int mid = (lt+rt) / 2;
-            for (int i=0;i<n; i++) {
-                sum += arr[i];
-                if (sum > mid) {
-                    list.add(sum - arr[i]);
-                    sum = 0;
-                    i = i-1;
-                }
-                if (i == n-1) {
-                    list.add(sum);
-                }
-            }
-            if (list.size() == m) {
-                if (result > Collections.max(list)) {
-                    result = Collections.max(list);
-                }
-                sum = 0;
-                rt = mid-1;
-                list = new ArrayList<>();
-            } else if (list.size() < m) {
-                sum = 0;
-                rt = mid-1;
-                list = new ArrayList<>();
+            mid = (lt + rt) / 2;
+            int checkCnt = check(mid, arr);
+
+            if (checkCnt <= m) { // checkcnt가 3개가 나왔다 근데 m개가 용량이다. 꽉꽉 채워서 3개가 들어가는거니까 답에 들어갈 수 있다.
+                rt = mid - 1;
+                result = mid;
             } else {
-                sum = 0;
-                lt = mid+1;
-                list = new ArrayList<>();
+                lt = mid + 1;
             }
         }
         return result;
+    }
+
+    private static int check(int mid, int[] arr) {
+        int sum = 0;
+        int cnt = 0;
+        for (int x : arr) {
+            if (sum + x > mid) {
+                sum = 0;
+                cnt++;
+            }
+            sum += x;
+        }
+        return cnt+1;
     }
 }
