e58ea2056c68737170b9d8d6e59199ee2617befe
eunjis <ejejc@naver.com>
2023-03-26 15:21:53 +0900

    팩토리얼 알고리즘 풀이.

---
+++ src/main/java/com/example/inflearn_algorithm/recursive_tree_graph/Ch08_03.java
@@ -0,0 +1,22 @@
+package com.example.inflearn_algorithm.recursive_tree_graph;
+
+public class Ch08_03 {
+    /**
+     * 재귀 알고리즘
+     * 자연수 N이 입력되면 N!를 구하는 프로그램을 작성하세요.
+     * 입력: 5
+     * 출력: 5*4*3*2*1 = 120
+     */
+
+    public static void main(String[] args) {
+        DfsFactorial(5);
+    }
+
+    private static int DfsFactorial(int n) {
+        if (n == 1) return 1;
+        else {
+            return n * DfsFactorial(n-1);
+        }
+    }
+
+}
