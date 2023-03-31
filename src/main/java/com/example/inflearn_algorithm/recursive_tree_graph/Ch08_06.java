a2d70f8cdc39f2c356f0d44cd676daed8512656d
eunjis <ejejc@naver.com>
2023-03-31 23:07:57 +0900

    부분집합 구하기 DFS 알고리즘 풀이.

--- src/main/java/com/example/inflearn_algorithm/recursive_tree_graph/Ch08_06.java
+++ src/main/java/com/example/inflearn_algorithm/recursive_tree_graph/Ch08_06.java
@@ -1,8 +1,5 @@
 package com.example.inflearn_algorithm.recursive_tree_graph;
 
-import com.example.inflearn_algorithm.recursive_tree_graph.Ch08_05.Main;
-import com.example.inflearn_algorithm.recursive_tree_graph.Ch08_05.Node;
-
 public class Ch08_06 {
     /**
      * 부분집합 구하기 - DFS
@@ -13,26 +10,30 @@ public class Ch08_06 {
      * 출력 : 1 2 3 / 1 2 / 1 3 / 1 / 2 3 / 2 / 3
      */
 
-    public static class Main {
-        int n;
-        int[] ch;
-        public void DFS(int L) {
-            if (L == n+1) {
+        static int n;
+        static int[] ch;
 
+        public void DFS(int L) {
+            if (L == n + 1) {
+                String tmp = "";
+                for (int i = 1; i <= n; i++) {
+                    if (ch[i] == 1) tmp += (i + " ");
+                }
+                if (tmp.length() > 0) System.out.println(tmp);
             } else {
                 ch[L] = 1;
-                DFS(L+1); // 사용한다 라고 뻗는 함수
+                DFS(L + 1); // 사용한다 라고 뻗는 함수
                 ch[L] = 0;
-                DFS(L+1);
+                DFS(L + 1);
             }
         }
 
-        public void main(String[] args) {
-            Main T  = new Main();
-            this.n = 3;
+        public static void main(String[] args) {
+            Ch08_06 T  = new Ch08_06();
+            n = 3;
             ch = new int[n+1];
             T.DFS(1);
         }
-    }
+
 
 }
